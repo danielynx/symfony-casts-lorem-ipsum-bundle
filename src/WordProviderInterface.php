5b29a99651483931a3f46f8aaff5bfaa75d0cb4e
Daniel Backes <danielbackes@protonmail.com>
2021-07-13 16:21:38 -0400

    feat: starter bundle project

---
+++ src/WordProviderInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Knpu\LoremIpsumBundle;
+
+interface WordProviderInterface
+{
+  /**
+  * Returns an array of words to use for fake text.
+  *
+  * @return array
+  */
+  public function getWordList(): array;
+}
